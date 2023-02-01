00385534b7197fc9ad6aa158565c06ed5988ca08
Buffik <62429354+Buffik@users.noreply.github.com>
2023-02-01 20:35:39 +0300

    Data fix (#4)
    * feat: add static data
    
    * fix: data structure

--- src/assets/data/users.ts
+++ src/assets/data/users.ts
@@ -1,48 +1,56 @@
 export type TUser = {
-  id: number;
-  key: string;
+  _id: number;
   data: {
     name: string;
     patronymic: string;
     surname: string;
     birthday: string;
+    mail: string;
+    phone: string;
+    password: string;
   };
-  isAdmin: boolean;
+  role: 'admin' | 'manager' | 'salesman';
 };
 
 const users: TUser[] = [
   {
-    id: 1,
-    key: 'someFlagToCheckUser_1',
+    _id: 1,
     data: {
       name: 'Владимир',
       patronymic: 'Сергеевич',
       surname: 'Буф',
       birthday: '06.04.1989',
+      mail: 'boo@mail.ru',
+      phone: '+7-921-591-77-77',
+      password: '123321123',
     },
-    isAdmin: true,
+    role: 'admin',
   },
   {
-    id: 2,
-    key: 'someFlagToCheckUser_2',
+    _id: 2,
     data: {
       name: 'Дарья',
       patronymic: 'Михайловна',
       surname: 'Буф',
       birthday: '17.09.1989',
+      mail: 'darya@mail.ru',
+      phone: '+7-922-222-22-22',
+      password: 'string',
     },
-    isAdmin: false,
+    role: 'manager',
   },
   {
-    id: 3,
-    key: 'someFlagToCheckUser_3',
+    _id: 3,
     data: {
       name: 'Олег',
       patronymic: 'Батькович',
       surname: 'Фамилия',
       birthday: '29.09.1989',
+      mail: 'oleg@mail.ru',
+      phone: '+7-999-999-99-99',
+      password: 'string123',
     },
-    isAdmin: false,
+    role: 'salesman',
   },
 ];
 
