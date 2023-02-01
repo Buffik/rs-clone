11ad3ac6393d240d8e19a09d09525f79597faa74
olegpl1993 <olegpl1993@gmail.com>
2023-02-01 22:30:32 +0200

    feat: validation mail and pass

--- src/pages/AuthorizationPage/AuthorizationPage.tsx
+++ src/pages/AuthorizationPage/AuthorizationPage.tsx
@@ -5,6 +5,22 @@ import styles from './Authorization.module.scss';
 function AuthorizationPage() {
   const [mailer, setMailer] = useState('');
   const [password, setPassword] = useState('');
+  const [mailerError, setMailerError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
+  const onChangeMailer = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setMailer(event.target.value);
+    const validMailer = !!mailer.match(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{1,}\b/);
+    if (validMailer) setMailerError(true);
+    else setMailerError(false);
+  };
+
+  const onChangePassword = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setPassword(event.target.value);
+    const validPassword = !!password.match(/[0-9a-zA-Z!@#$%^&*]{4,}/);
+    if (validPassword) setPasswordError(true);
+    else setPasswordError(false);
+  };
 
   const authorization = () => {
     const authorizationObj = {
@@ -23,16 +39,18 @@ function AuthorizationPage() {
             label="почта"
             variant="outlined"
             size="medium"
-            onChange={(event) => { setMailer(event.target.value); }}
+            color={mailerError ? 'primary' : 'error'}
+            onChange={(event) => onChangeMailer(event)}
           />
           <TextField
             id="password"
             label="пароль"
             variant="outlined"
             size="medium"
-            onChange={(event) => { setPassword(event.target.value); }}
+            color={passwordError ? 'primary' : 'error'}
+            onChange={(event) => onChangePassword(event)}
           />
-          <Button variant="contained" onClick={authorization}>Войти</Button>
+          <Button variant="contained" onClick={authorization} disabled={!(mailerError && passwordError)}>Войти</Button>
         </div>
       </Paper>
     </div>
