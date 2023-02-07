65569206b50f560d0c9c14b2faa5f5f99140a412
olegpl1993 <olegpl1993@gmail.com>
2023-02-07 02:14:46 +0200

    feat: select month week

--- src/pages/CalendarPage/CalendarPage.tsx
+++ src/pages/CalendarPage/CalendarPage.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
-import { Paper } from '@mui/material';
+import React, { useState } from 'react';
+import Paper from '@mui/material/Paper';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 import styles from './CalendarPage.module.scss';
 
 function CalendarPage() {
+  const [show, showAge] = useState('month');
+  const showChange = (event: SelectChangeEvent) => showAge(event.target.value);
+
   return (
     <Paper elevation={5} className={styles.calendar}>
-      CalendarPage
+      <div className={styles.setBox}>
+        <div className={styles.yearMonthRow}>
+          <div className={styles.year}>2023</div>
+          <div className={styles.month}>Февраль</div>
+        </div>
+        <div className={styles.selectRow}>
+          <div className={styles.show}>Show:</div>
+          <Select
+            value={show}
+            onChange={showChange}
+            sx={{ minWidth: 100, height: 30 }}
+          >
+            <MenuItem value="month">Month</MenuItem>
+            <MenuItem value="week">Week</MenuItem>
+          </Select>
+        </div>
+      </div>
+      <div className={styles.divider} />
+      <div className={styles.calendarBox}>Блок календаря</div>
     </Paper>
   );
 }
