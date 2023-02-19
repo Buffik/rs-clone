251c63b4df0ecec7e651a75c9ff36491d4943ae1
olegpl1993 <olegpl1993@gmail.com>
2023-02-19 20:32:30 +0200

    feat: create open modal windows

--- src/pages/ContactsListPage/ContactsListPage.tsx
+++ src/pages/ContactsListPage/ContactsListPage.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Paper,
   IconButton,
+  Modal,
 } from '@mui/material';
 import ModeIcon from '@mui/icons-material/Mode';
 import ControlPointIcon from '@mui/icons-material/ControlPoint';
@@ -9,15 +10,15 @@ import { useAppSelector } from '../../hook';
 import LoadingSpinner from '../../components/UI/Spinner/LoadingSpinner';
 import styles from './ContactsListPage.module.scss';
 
-const addContact = () => {
-  console.log('new Contact');
-};
-
-const editContact = () => {
-  console.log('edit contact');
-};
-
 function ContactsListPage() {
+  const [openAdd, setOpenAdd] = useState(false);
+  const handleOpenAdd = () => setOpenAdd(true);
+  const handleCloseAdd = () => setOpenAdd(false);
+
+  const [openEdit, setOpenEdit] = useState(false);
+  const handleOpenEdit = () => setOpenEdit(true);
+  const handleCloseEdit = () => setOpenEdit(false);
+
   const { contacts } = useAppSelector((state) => state.data);
   console.log(contacts);
 
@@ -26,37 +27,63 @@ function ContactsListPage() {
   }
 
   return (
-    <Paper elevation={4} className={styles.contactsListPage}>
-      <div className={styles.topRow}>
-        <div className={styles.topName}>Name</div>
-        <div className={styles.topCompanyName}>Company name</div>
-        <div className={styles.topMail}>Email</div>
-        <div className={styles.topPhone}>Phone</div>
-        <div className={styles.topBtn}>
-          <IconButton onClick={addContact}>
-            <ControlPointIcon fontSize="large" />
-          </IconButton>
+    <>
+      <Modal
+        open={openAdd}
+        onClose={handleCloseAdd}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        className={styles.modalWrapper}
+      >
+        <div className={styles.modalContent}>
+          add contacts
         </div>
-      </div>
-      {contacts && contacts.map((contact) => (
-        <div className={styles.contactBox}>
-          <div className={styles.divider} />
-          { /* eslint-disable-next-line no-underscore-dangle */}
-          <div key={contact._id} className={styles.row}>
-            {/* eslint-disable-next-line max-len */}
-            <div className={styles.name}>{contact.firstName} {contact.surname} {contact.patronymic}</div>
-            <div className={styles.companyName}>{contact.companyName}</div>
-            <div className={styles.mail}>{contact.mail}</div>
-            <div className={styles.phone}>{contact.phone}</div>
-            <div className={styles.btn}>
-              <IconButton onClick={editContact}>
-                <ModeIcon />
-              </IconButton>
-            </div>
+      </Modal>
+
+      <Modal
+        open={openEdit}
+        onClose={handleCloseEdit}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        className={styles.modalWrapper}
+      >
+        <div className={styles.modalContent}>
+          edit contacts
+        </div>
+      </Modal>
+
+      <Paper elevation={4} className={styles.contactsListPage}>
+        <div className={styles.topRow}>
+          <div className={styles.topName}>Name</div>
+          <div className={styles.topCompanyName}>Company name</div>
+          <div className={styles.topMail}>Email</div>
+          <div className={styles.topPhone}>Phone</div>
+          <div className={styles.topBtn}>
+            <IconButton onClick={handleOpenAdd}>
+              <ControlPointIcon fontSize="large" />
+            </IconButton>
           </div>
         </div>
-      ))}
-    </Paper>
+        {contacts && contacts.map((contact) => (
+          <div className={styles.contactBox}>
+            <div className={styles.divider} />
+            { /* eslint-disable-next-line no-underscore-dangle */}
+            <div key={contact._id} className={styles.row}>
+              {/* eslint-disable-next-line max-len */}
+              <div className={styles.name}>{contact.firstName} {contact.surname} {contact.patronymic}</div>
+              <div className={styles.companyName}>{contact.companyName}</div>
+              <div className={styles.mail}>{contact.mail}</div>
+              <div className={styles.phone}>{contact.phone}</div>
+              <div className={styles.btn}>
+                <IconButton onClick={handleOpenEdit}>
+                  <ModeIcon />
+                </IconButton>
+              </div>
+            </div>
+          </div>
+        ))}
+      </Paper>
+    </>
   );
 }
 
