1ac9b8d6608afd5f2d7bb1e0de28a401ba7fe2b8
olegpl1993 <olegpl1993@gmail.com>
2023-02-19 21:36:21 +0200

    feat: name validation

--- src/pages/ContactsListPage/AddContactModal/AddContactModal.tsx
+++ src/pages/ContactsListPage/AddContactModal/AddContactModal.tsx
@@ -3,6 +3,13 @@ import React, { useState } from 'react';
 import styles from './AddContactModal.module.scss';
 
 function AddContactModal() {
+  const [name, setName] = useState('');
+  const [nameError, setNameError] = useState(false);
+  const onChangeName = (value: string) => {
+    setName(value);
+    setNameError((!value.match(/^[a-zA-Zа-яА-Я]{2,20}?$/u)) && value !== '');
+  };
+
   const [mailer, setMailer] = useState('');
   const [mailerError, setMailerError] = useState(false);
   const onChangeMailer = (value: string) => {
@@ -13,6 +20,18 @@ function AddContactModal() {
   return (
     <div className={styles.modalContent}>
       <div className={styles.modalName}>Add contact</div>
+      <TextField
+        autoComplete="off"
+        sx={{ width: '220px' }}
+        error={nameError}
+        id="name"
+        label="name"
+        variant="outlined"
+        size="medium"
+        value={name}
+        onChange={(event) => onChangeName(event.target.value)}
+        helperText={mailerError ? 'incorrect' : ' '}
+      />
       <TextField
         autoComplete="off"
         sx={{ width: '220px' }}
