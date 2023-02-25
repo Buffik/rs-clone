f917ddb2c6efddea9c610eb476a304aa2ef37d4f
Buffik <Som16@mail.ru>
2023-02-25 08:53:56 +0300

    refactor: getting current day data

--- src/components/DraggableItem/DroppableArea/DroppableArea.tsx
+++ src/components/DraggableItem/DroppableArea/DroppableArea.tsx
@@ -74,11 +74,8 @@ const areaData: TAreaData[] = [
 
 export default function DroppableArea() {
   const date = new Date();
-  const normalizedDate = `${date.getFullYear()}-${String(
-    date.getUTCMonth() + 1,
-  ).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+  const normalizedDate = date.toISOString().split('T')[0];
   const currentDay = useAppSelector((state) => state.selectedDay.selectedDay);
-  console.log(currentDay);
   const HEIGHT_PER_HALF_HOUR = 44;
   const MAX_TODO_WIDTH = 80;
   const MIN_TIME_TODO_LENGTH = 30;
@@ -109,7 +106,6 @@ export default function DroppableArea() {
   });
 
   useEffect(() => {
-    console.log(currentDay);
     fetchTodos();
   }, []);
 
